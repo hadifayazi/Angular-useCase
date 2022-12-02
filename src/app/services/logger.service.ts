d3b1948f7fd2e7317a9cc4376e7374ba69d2171f
hadifayazi <hadifayazi4@gmail.com>
2022-12-02 14:30:43 +0100

    creating logger service

---
+++ src/app/services/logger.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggerService {
+  logs: string[] = [];
+
+  constructor() {}
+  log(msg: string) {
+    this.logs.push(msg);
+    console.log(msg);
+  }
+}
