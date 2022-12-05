08c3458138ab69f86b7b896b65138d0babdaeca9
hadifayazi <hadifayazi4@gmail.com>
2022-12-05 19:39:48 +0100

    adding room model, 2way databinding using ngModel on the form inputs

--- src/app/rooms/add-room/add-room.component.ts
+++ src/app/rooms/add-room/add-room.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomList } from '../roomInterface';
 
 @Component({
   selector: 'app-add-room',
   templateUrl: './add-room.component.html',
-  styleUrls: ['./add-room.component.css']
+  styleUrls: ['./add-room.component.css'],
 })
 export class AddRoomComponent implements OnInit {
+  room: RoomList = {
+    roomType: '',
+    roomNumber: 0,
+    ameneties: '',
+    price: 0,
+    image: '',
+    checkinTime: new Date(),
+    checkoutTime: new Date(),
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
