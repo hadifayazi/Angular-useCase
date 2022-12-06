9454508fb2fbdb7f8245ca453699e2938fa151a0
hadifayazi <hadifayazi4@gmail.com>
2022-12-06 21:29:16 +0100

    subscribing to a pariticular moment of the route events, individually instead of listening to the entire process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  GuardsCheckEnd,
+  NavigationEnd,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 import { LocoalStorageToken } from './services/localstorage.token';
 import { RoomsService } from './services/rooms.service';
+import { filter, pipe } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,9 +24,28 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.events.subscribe((event) => {
-      console.log(event);
-    });
+    // this.route.events.subscribe((event) => {
+    //   console.log(event);
+    // });
+
+    this.route.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe((event) => {
+        console.log(
+          '*********************Navigation started*******************'
+        );
+      });
+    this.route.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        console.log('***********Navoigatiopon ended**************');
+      });
+
+    this.route.events
+      .pipe(filter((event) => event instanceof GuardsCheckEnd))
+      .subscribe((event) => {
+        console.log(event);
+      });
     this.storageToken.localStorage = 'My app Token';
   }
 }
