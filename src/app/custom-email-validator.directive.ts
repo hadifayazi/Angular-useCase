6cdd680ef67f5e69e17a22c56a81a4c7230fb6a6
hadifayazi <hadifayazi4@gmail.com>
2022-12-06 13:11:26 +0100

    our custom validator implements class validator, Angular has ints nativ validators, so we have to add our cutom validator to the validators, we do by it adding providers array, the value of provide will be angular NG_VALIATORS token, using our costum class, and because its an array we add mutli to true. Then we should add the selector of our validator to the component, to get the value of the input we asigne control to ngModel, and check if there is an error

--- src/app/custom-email-validator.directive.ts
+++ src/app/custom-email-validator.directive.ts
@@ -1,10 +1,30 @@
 import { Directive } from '@angular/core';
+import {
+  AbstractControl,
+  ValidationErrors,
+  Validator,
+  NG_VALIDATORS,
+} from '@angular/forms';
 
 @Directive({
-  selector: '[appCustomEmailValidator]'
+  selector: '[appCustomEmailValidator]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: CustomEmailValidatorDirective,
+      multi: true,
+    },
+  ],
 })
-export class CustomEmailValidatorDirective {
-
-  constructor() { }
-
+export class CustomEmailValidatorDirective implements Validator {
+  constructor() {}
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const value = control.value as string;
+    if (value.includes('test')) {
+      return {
+        invalidEmail: true,
+      };
+    }
+    return null;
+  }
 }
