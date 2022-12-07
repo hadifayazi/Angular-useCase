f3cd0f2aedc2298a6ca0ff77f6f8f3cc62b6033f
hadifayazi <hadifayazi4@gmail.com>
2022-12-07 14:19:34 +0100

    first creating a booking form formGroup, then we inject the form builder to builer the form from the group that we've defined

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css']
+  styleUrls: ['./booking.component.css'],
 })
 export class BookingComponent implements OnInit {
+  bookingForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.bookingForm = this.formBuilder.group({
+      bookingId: new FormControl(''),
+      roomId: new FormControl(''),
+      bookingDate: new FormControl(''),
+      numberOfGuests: new FormControl(''),
+      bookingPrice: new FormControl(''),
+      bookingStatus: new FormControl(''),
+      checkinTime: new FormControl(''),
+      checkoutTime: new FormControl(''),
+      guestEmail: new FormControl(''),
+      guestName: new FormControl(''),
+      guestAdress: new FormControl(''),
+      guestPhone: new FormControl(''),
+    });
   }
-
 }
