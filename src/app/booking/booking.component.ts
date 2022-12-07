dc103050b2d94dc6eb3733f8489c0e1d155b3b2d
hadifayazi <hadifayazi4@gmail.com>
2022-12-07 19:44:58 +0100

    updating form fileds, basic validation, adding form template to the view

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
@@ -14,17 +19,34 @@ export class BookingComponent implements OnInit {
   ngOnInit(): void {
     this.bookingForm = this.formBuilder.group({
       bookingId: new FormControl(''),
+
       roomId: new FormControl(''),
-      bookingDate: new FormControl(''),
-      numberOfGuests: new FormControl(''),
+
+      bookingDate: new FormControl('', [Validators.required]),
+
+      numberOfGuests: new FormControl('', [Validators.required]),
+
       bookingPrice: new FormControl(''),
+
       bookingStatus: new FormControl(''),
-      checkinTime: new FormControl(''),
-      checkoutTime: new FormControl(''),
-      guestEmail: new FormControl(''),
-      guestName: new FormControl(''),
-      guestAdress: new FormControl(''),
-      guestPhone: new FormControl(''),
+
+      checkinTime: new FormControl('', [Validators.required]),
+
+      checkoutTime: new FormControl('', [Validators.required]),
+
+      guestEmail: new FormControl('', [Validators.required]),
+
+      guestName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+
+      guestAdress: new FormControl('', [Validators.required]),
+
+      guestPhone: new FormControl('', [
+        Validators.required,
+        Validators.min(10),
+      ]),
     });
   }
 }
