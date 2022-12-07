5420d4b0b7e7f543484c24ebd155401a5cf576ff
hadifayazi <hadifayazi4@gmail.com>
2022-12-07 21:39:11 +0100

    making the value of Input type dynamic

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -8,6 +8,7 @@ import { FormControl } from '@angular/forms';
 })
 export class InputComponent implements OnInit {
   @Input() control: FormControl = new FormControl();
+  @Input() type: string = '';
 
   constructor() {}
 
