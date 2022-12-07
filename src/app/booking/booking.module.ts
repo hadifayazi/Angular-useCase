a80e5a75e4bc6c74b683ae530499593708468a3e
hadifayazi <hadifayazi4@gmail.com>
2022-12-07 13:27:10 +0100

    creating reactiv form so user will be able to book a room, first we add ReactiveFormsModule to our bookingModule

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from './booking.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    BookingComponent
-  ],
-  imports: [
-    CommonModule,
-    BookingRoutingModule
-  ]
+  declarations: [BookingComponent],
+  imports: [CommonModule, BookingRoutingModule, ReactiveFormsModule],
 })
-export class BookingModule { }
+export class BookingModule {}
