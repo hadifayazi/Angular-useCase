ddc720c8b3f6b7bcebede482d8564aed40253179
hadifayazi <hadifayazi4@gmail.com>
2022-12-08 14:32:40 +0100

    making call, injecting comment sservice, adding html tablle to the view

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentService } from './comment.service';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  styleUrls: ['./comment.component.css'],
 })
 export class CommentComponent implements OnInit {
+  comments$ = this.commentService.getComment();
+  constructor(private commentService: CommentService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/comment/comment.service.ts
+++ src/app/comment/comment.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Comments } from './comment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getComment() {
+    return this.http.get<Comments[]>(
+      'https://jsonplaceholder.typicode.com/comments'
+    );
+  }
 }
